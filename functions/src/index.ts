ce6f1d39ad5286cb91cf5011206e6220e18454b5
Amijul <amijulmondal@gmail.com>
2025-12-17 22:23:19 +0530

    firebase Fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,16 +5,13 @@ admin.initializeApp();
 
 export const onAuthUserCreate = functions.auth.user().onCreate(
   async (user: admin.auth.UserRecord) => {
-    const uid = user.uid;
-
-    await admin.firestore().collection("users").doc(uid).set(
+    await admin.firestore().collection("users").doc(user.uid).set(
       {
         role: "buyer",
         status: "active",
       },
       { merge: true }
     );
-
     return null;
   }
 );
