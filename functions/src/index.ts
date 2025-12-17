d4e7f6f20486ae10d43058740589a1531cd3fbe2
Amijul <amijulmondal@gmail.com>
2025-12-17 22:34:14 +0530

    Fix eslint comma spacing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ admin.initializeApp();
 export const onAuthUserCreate = functions.auth.user().onCreate(
   async (user: admin.auth.UserRecord) => {
     await admin.firestore().collection("users").doc(user.uid).set(
-      {role: "buyer",status: "active"},
+      {role: "buyer", status: "active"},
       {merge: true}
     );
     return null;
